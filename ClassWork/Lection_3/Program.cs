8370779ac403561669aa97d68a78710f25b500e1
Ann <pgav89@mail.ru>
2022-07-12 11:43:21 +1000

    sem5

--- ClassWork/Lection_3/Program.cs
+++ ClassWork/Lection_3/Program.cs
@@ -171,21 +171,65 @@
 
 
 //Двумерные массивы:
+// int [,]matrix=new int[5,8];
+// string[,]table=new string[2,5]; //table[0,0]-1 столбик,1 строки table[0,1]-2 столбик, 1 строки...table[0,4] 4столб,1строки
+// table[1,2]="слово ";            //table[1,0]-1 столбик,2 строки table[1,1]-2 столбик,2 строки ...table[4,4] 4столб 2 строки
+// for(int rows=0; rows<2; rows++)
+// {
+//     for(int columns=0; columns<5;columns++)
+//     {
+//         Console.WriteLine($"-{table[rows,columns]}-");
+//     }
+// }
+
+ int[,] matrix = new int[3, 4];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($"{matrix[i, j]}");
+//     }
+//     Console.WriteLine();
+// }
 
 
-
+ void PrintArray(int[,], matr)
+ {
+     for (int i = 0; i < matr.GetLength(0); i++)
+     {
+         for (int j = 0; j < matr.GetLength(1); j++)
+         {
+             Console.Write($"{matr[i, j]}");
+         }
+         Console.WriteLine();
+     }
+ }
+ void FillArray(int[,] matr)
+ {
+     for (int i = 0; i < matr.GetLength(0); i++)
+     {
+         for (int j = 0; j < matr.GetLength(1); j++)
+         {
+             matr[i, j] = new Random().Next(1, 10); //[1;10)
+         }
+     }
+ }
+ int[,] matrix = new int[3, 4];
+ PrintArray(matrix);
+ FillArray(matrix) < Console.WriteLine();
+ PrintArray(matrix);
 
 
 //КАК ПРЕДСТАВИТЬ ЧЕРНО-БЕЛОЕ ИЗОБРАЖЕНИЕ:
-// void PrintImage(int[,], image)
+//  void PrintImage(int[,], image)
 // {
-//     for (int i = 0; i < image.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < image.GetLength(1); i++)
+// for (int i = 0; i < image.GetLength(0); i++)
+//  {
+//         for (int j = 0; j < image.GetLength(1); j++)
 //         {
-//             if (image[i, j] == 0) Console.Write($" ");
-//             else (Console.Write($"+"));
+//            if (image[i, j] == 0) Console.Write($" ");
+//           else (Console.Write($"+"));
 //         }
-//     }
+//      }
 // }
-// PrintImage(pic);
+//  PrintImage(pic);
---
+++ Seminar/Sem_5/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный 
+//случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных 
+//и положительных элементов массива.
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+//сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+// int[] array = new int[12];
+
+// int size = array.Length;
+
+// int resultPositive = 0;
+
+// int resultNegative = 0;
+
+// int index = 0;
+
+// while (index < size) 
+// {
+//     array[index] = new Random().Next(-9,10); // [-9;10)
+//     index++;
+// }
+// Console.WriteLine($"[{String.Join("; ", array)}]");
+// for (int startIndex = 0; startIndex < size; startIndex++)
+// {
+//     if (array[startIndex] > 0) 
+//     {
+//         resultPositive += array[startIndex];
+//         //resultPositive=resultPositive +  array[startIndex]
+//     }
+//     else 
+//     {
+//         resultNegative += array[startIndex];
+//     }
+// }
+
+
+// Console.WriteLine($"Сумма положительных чисел = {resultPositive}");
+// Console.WriteLine($"Сумма отрицательных чисел = {resultNegative}");
+
+
+// for (int i = 0; i < n; i++)   
+//  {                 
+//     if (x[i] != chislo) 
+//      {                 
+//         Console.WriteLine("Такого числа нет!!!");
+//         Console.ReadKey();  
+//         return;  
+//     }    
+//  }  
